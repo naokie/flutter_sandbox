a7eea40391f2914242c6212b9adfceee7c7fece5
Naoki Endoh <naoki.endoh@gmail.com>
2018-04-11 21:27:08 +0900

    Add external package

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          // child: new Text('Hello World')
+          child: new Text(wordPair.asPascalCase),
+        )
+      )
     );
   }
 }
